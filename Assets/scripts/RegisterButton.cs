62466c93dd418cbc5cdff71f5a52b945b6d5c23d
aliluczak <aliluczak@gmail.com>
2015-07-16 15:25:27 +0200

    v 1.18
    poprawione działanie przycisków menu

---
+++ Assets/scripts/RegisterButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RegisterButton : MonoBehaviour {
+
+    [SerializeField]
+    private Button confirm, back = null;
+    private Menu menu;
+    private GameObject inputs;
+    private Text nickname, password, confirmPassword;
+
+
+	// Use this for initialization
+	void Start () {
+        menu = GameObject.Find("Menu").GetComponent<Menu>();
+        inputs = GameObject.Find("NickNameText");
+        nickname = inputs.GetComponent<Text>();
+        inputs = GameObject.Find("PasswordText");
+        password = inputs.GetComponent<Text>();
+        inputs = GameObject.Find("RepeatPasswordText");
+        confirmPassword = inputs.GetComponent<Text>();
+
+        confirm.onClick.AddListener(() => menu.registrationStepTwo());
+        back.onClick.AddListener(() => menu.backButton());
+	}
+	
+	
+}
